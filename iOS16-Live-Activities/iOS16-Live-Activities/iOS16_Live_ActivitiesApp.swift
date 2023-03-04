498121987e1c597b6e46cf91172b23f911e799f1
adityavtridhya <vaghelaaditya007@gmail.com>
2023-03-04 19:10:04 +0530

    - Pizza ordered

---
+++ iOS16-Live-Activities/iOS16-Live-Activities/iOS16_Live_ActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16_Live_ActivitiesApp.swift
+//  iOS16-Live-Activities
+//
+//  Created by MacBook Pro on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS16_Live_ActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
